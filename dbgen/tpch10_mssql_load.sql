f014445e5c34c5172dd9d4cc8742b1035dc66b05
sclai <pallasjack@outlook.com>
2025-04-30 15:10:36 +0800

    mod dbgen/tpch10_mssql_load.sql

--- dbgen/tpch10_mssql_load.sql
+++ dbgen/tpch10_mssql_load.sql
@@ -4,11 +4,11 @@
 -- USE TPCH_010
 -- GO
 
-BULK INSERT part     FROM '/home/sclai/Downloads/vm_shared/part.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT customer FROM '/home/sclai/Downloads/vm_shared/customer.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT orders   FROM '/home/sclai/Downloads/vm_shared/order.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT partsupp FROM '/home/sclai/Downloads/vm_shared/partsupp.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT supplier FROM '/home/sclai/Downloads/vm_shared/supplier.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT lineitem FROM '/home/sclai/Downloads/vm_shared/lineitem.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT nation   FROM '/home/sclai/Downloads/vm_shared/nation.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
-BULK INSERT region   FROM '/home/sclai/Downloads/vm_shared/region.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT part     FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/part.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT customer FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/customer.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT orders   FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/orders.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT partsupp FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/partsupp.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT supplier FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/supplier.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT lineitem FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/lineitem.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT nation   FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/nation.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
+BULK INSERT region   FROM '/home/sclai/project/DBABandits/tpch-kit/dbgen/region.tbl' WITH (TABLOCK, DATAFILETYPE='char', CODEPAGE='raw', FIELDTERMINATOR = '|')
